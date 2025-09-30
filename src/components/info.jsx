36c936741ddbfe4a8a2d72463e7d5d05be57ad78
Dhruv tailor <tailordhurv4005@gmail.com>
2025-09-30 15:39:44 +0530

    info

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { FaPhone, FaEnvelope, FaMapMarkedAlt, FaMoon, FaSun } from "react-icons/fa";
+import {
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkedAlt,
+  FaMoon,
+  FaSun,
+} from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -26,14 +32,14 @@ function Info() {
       data-aos="fade-down"
       className={`w-full h-auto ${
         darkMode ? "bg-gray-800" : "bg-purple-600"
-      } text-white py-3 px-5 flex justify-between md:flex-row md:justify-between md:items-center gap-4`}
+      } text-white py-3 px-5 flex overflow-x-hidden justify-between items-center`}
     >
       {/* Contact Info */}
-      <div className="flex flex-col md:flex-row md:gap-10 gap-4 items-start md:items-center">
+      <div className="flex-col md:flex gap-6 pl-5 items-center">
         <a
           data-aos="fade-right"
           data-aos-delay="200"
-          className="flex gap-2 items-center text-sm sm:text-base"
+          className="flex gap-2 items-center"
           href="tel:9521634141"
         >
           <FaPhone /> 9521634141
@@ -41,7 +47,7 @@ function Info() {
         <a
           data-aos="fade-up"
           data-aos-delay="400"
-          className="flex gap-2 items-center text-sm sm:text-base"
+          className="flex gap-2 items-center"
           href="mailto:tailor@gmail.com"
         >
           <FaEnvelope /> tailor@gmail.com
@@ -49,30 +55,26 @@ function Info() {
         <div
           data-aos="fade-left"
           data-aos-delay="600"
-          className="flex gap-2 items-center text-sm sm:text-base"
+          className="flex gap-2 items-center"
         >
           <FaMapMarkedAlt /> Kotputli
         </div>
       </div>
 
       {/* Dark Mode Button */}
-   <div
-  data-aos="zoom-in"
-  data-aos-delay="800"
-  className="flex justify-center md:justify-end"
->
-  <button
-    onClick={() => setDarkMode(!darkMode)}
-    className={`p-2 rounded-full border-2 ${
-      darkMode ? "border-white text-white" : "border-black text-black"
-    } hover:scale-110 transition duration-300`}
-  >
-    {darkMode ? <FaSun size={20} /> : <FaMoon size={18} />}
-  </button>
-</div>
-
+      <div data-aos="zoom-in" data-aos-delay="800">
+        <button
+          onClick={() => setDarkMode(!darkMode)}
+          className={`p-2 rounded-full border ${
+            darkMode ? "border-white" : "border-black"
+          } hover:scale-110 transition`}
+        >
+          {darkMode ? <FaSun size={18} /> : <FaMoon size={18} />}
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Info;
+
